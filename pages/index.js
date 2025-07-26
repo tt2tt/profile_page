d87ebbed3ecb48aed600257101d99721631a32d4
tt <groveleffect@gmail.com>
2025-07-26 19:05:31 +0900

    トップページの作成

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1>Top Page</h1>
+      <h1>Hello My name is Taro <br /> Welcome to My Profile Site</h1>
     </Layout>
   );
 }
