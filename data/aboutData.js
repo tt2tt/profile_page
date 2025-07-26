27c675a1a78d450b33b3b248ebe78fee48e6b78d
tt <groveleffect@gmail.com>
2025-07-26 14:02:03 +0900

    アバウトページの作成
    アバウトコンポーネント、データの追加

---
+++ data/aboutData.js
@@ -0,0 +1,12 @@
+const aboutData = {
+  name: "田中 太朗 (Taro Tanaka)",
+  university: "西九州大学 子ども部 子ども学科",
+  github: {
+    label: "tt2tt",
+    url: "https://github.com/tt2tt",
+  },
+  detail: "プログラミングスクールで学習後IT企業に入社し、Pythonを使用した自動化プログラムの作成やCMSを使用したWebサイトの作成を経験しています。常に新しい技術を1から学び業務を行ってきました。自身で考え周りと協力しながら作業を行うことができます。",
+  profileImage: "https://tt2tt-profile-page.web.app/img/profile_img.5a006b71.jpg",
+};
+
+export default aboutData;
