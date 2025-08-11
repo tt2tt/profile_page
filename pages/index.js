8ab4f2e2c17d92046fb69a7553f455ce71f399c1
tt <groveleffect@gmail.com>
2025-08-11 15:20:08 +0900

    globals.cssにスタイルを移動

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,7 @@ import Skill from './skill';
 export default function Home() {
   return (
     <Layout>
-      <section id="top" className="min-h-[calc(75vh+50px)] flex items-center justify-center bg-gray-100 mb-8"
-            style={{
-        backgroundImage: "url('/images/top_page_background.png')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}>
+      <section id="top">
         <Top />
       </section>
       <section id="about" className="min-h-[calc(75vh+50px)] flex items-center justify-center bg-gray-300 mt-8 mb-8">
