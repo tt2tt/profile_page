1621c70e0d46a8d7c9946d1b6388e5084d8f1bfb
tt <groveleffect@gmail.com>
2025-12-01 15:42:25 +0900

    スマホ表示のデザインを調整

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -1,7 +1,7 @@
 export default function PortfolioItem({ image, title, description, lang, websiteLink, githubLink }) {
   return (
-    <div className="col-span-12 sm:col-span-8 md:col-span-4 lg:col-span-4 xl:col-span-3 m-4">
-      <li className="card border border-gray-300 rounded-lg shadow-sm bg-gray-100">
+    <div className="col-span-12 sm:col-span-8 md:col-span-4 lg:col-span-4 xl:col-span-3">
+      <li className="card border border-gray-300 rounded-lg shadow-sm bg-gray-100 mx-auto">
         <img src={image} alt={title} className="card-img w-full h-48 object-cover rounded-t-lg" />
         <div className="m-4">
           <p className="card-title text-lg font-bold mt-2">{title}</p>
--- components/top.js
+++ components/top.js
@@ -3,12 +3,12 @@ import Layout from './Layout';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white text-center text-5xl fade-in">
-        <div className="typewriter-line1">Hello. My name is Taro</div>
+      <h1 className="text-white text-center text-3xl sm:text-5xl fade-in">
+        <div className="typewriter-line1 text-xl sm:text-4xl">Hello. My name is Taro</div>
         <br />
-        <div className="typewriter-line2 mt-5">Welcome to my profile site</div>
+        <div className="typewriter-line2 mt-5 text-xl sm:text-4xl">Welcome to my profile site</div>
       </h1>
-      <p className="text-white text-center text-4xl mt-12 fade-in-p blink">Please scroll</p>
+      <p className="text-white text-center text-2xl sm:text-4xl mt-12 fade-in-p blink">Please scroll</p>
     </div>
   );
 }
